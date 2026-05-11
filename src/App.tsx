e30a1013ab53ab662ab6810f9f8f5ea8bb91151f
Samnang012 <unfake.nang168@gmail.com>
2026-05-11 16:14:42 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+import Hero from "./Components/Home/Hero";
+
+function App() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Home/Hero.tsx
@@ -0,0 +1,9 @@
+// import React from "react";
+
+export default function Hero() {
+  return (
+    <div>
+      <h1 className="bg-blue-600 text-white p-4">Tailwind Working</h1>
+    </div>
+  );
+}
