37a7384ccf94fe63ced78a58ec190b2ba1d75528
Samnang012 <unfake.nang168@gmail.com>
2026-05-12 13:13:45 +0700

    first commit

--- src/Components/Home/Hero.tsx
+++ src/Components/Home/Hero.tsx
@@ -3,7 +3,7 @@
 export default function Hero() {
   return (
     <div>
-      <h1 className="bg-blue-600 text-white p-4">Tailwind Working</h1>
+      <h1 className="bg-blue-600 text-white p-4">Tailwind Working gg</h1>
     </div>
   );
 }
